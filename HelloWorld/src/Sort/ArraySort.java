5e82e1aef68250c89766623b5416f7cdddc10b29
Bobochung <zb911318@gmail.com>
2017-09-11 10:22:52 +0800

    Update ArraySort.java
    update mergeSort

--- HelloWorld/src/Sort/ArraySort.java
+++ HelloWorld/src/Sort/ArraySort.java
@@ -72,60 +72,49 @@ public int[] insertSort(int[] arr){
 
 	
 //	归并排序
-	public void merge(int[] array,int start,int mid,int end){
-		int temp[] = new int[end-start+1];//临时数组  
-	      
-	    int firstArrIndex = start;//第一段数组序列的下标  
-	    int secondArrIndex = mid+1;//第二段数组序列的下标  
-	    int tempArrIndex = 0;//临时存放数组的下标  
-	      
-	    //1.扫描第一个数组序列和第二个数组序列  
-	    while(firstArrIndex <=mid && secondArrIndex<=end){  
-	        //1.1 当第一段数组小于第二段数组 未排序的首个元素时  
-	        if(array[firstArrIndex] <=array[secondArrIndex]){  
-	            temp[tempArrIndex] = array[firstArrIndex];  
-	            firstArrIndex++;  
-	        }else{//1.2 当第二段数组小于第一段数组 未排序的首个元素时  
-	            temp[tempArrIndex] = array[secondArrIndex];  
-	            secondArrIndex++;  
-	        }  
-	          
-	        tempArrIndex++;  
-	    }  
-	      
-	    //2.当第一段没有复制完全时，将剩余的数组全部复制到临时数组  
-	    while(firstArrIndex<=mid){  
-	        temp[tempArrIndex] = array[firstArrIndex];  
-	        firstArrIndex++;  
-	        tempArrIndex++;  
-	    }  
-	            
-	    //3.当第二段没有复制完全时，讲剩余的数组全部复制到临时数组  
-	    while(secondArrIndex<=end){  
-	        temp[tempArrIndex] = array[secondArrIndex];  
-	        secondArrIndex++;  
-	        tempArrIndex++;  
-	    }  	      
-	    //4.将临时数组复制到原始数组  
-	    for(tempArrIndex=0,firstArrIndex=start;firstArrIndex<=end;tempArrIndex++,firstArrIndex++){  
-	        array[firstArrIndex] = temp[tempArrIndex];  
-	    }  	
+		public static void mergeSort(int[] arr,int left,int right){
+		int middle=(left+right)/2;
+		if(left<right){
+//			让左边继续拆
+			mergeSort(arr, left, middle);
+//			让右边继续拆
+			mergeSort(arr, middle+1, right);
+//			合并两个有序数组为一个数组
+			merge(arr, left,middle, right);
+		}
 	}
-	public void mergeSort(int[] arr){
-		for (int gap = 1; gap < arr.length; gap = 2 * gap) {  
-	        int i=0;  
-	        //归并gap长度的两个相邻子数组  
-	        for(i=0; i+2*gap-1< arr.length; i = i + 2*gap) {  
-	            merge(arr, i, i+gap-1, i+2*gap-1);  
-	        }  
-	          
-	        // 余下不足两个合并的子数组。保证第一个数组gap存在。  
-	        if(i + gap - 1 < arr.length){  
-	            merge(arr, i, i + gap - 1, arr.length - 1);  
-	        }  
-	    }  
+	
+	public static void merge(int[] arr,int left,int middle,int right){
+//		定义临时数组
+		int[] temp=new int[right-left+1];
+		int low1=left;
+		int low2=middle+1;
+		int index=0;
+		 while (low1 <= middle && low2 <= right) {
+	            if (arr[low1] <= arr[low2]) {
+	                temp[index] = arr[low1++];
+	                index++;
+	            } else {
+	                temp[index] = arr[low2++];
+	                index++;
+	            }
+	        }
+	        // 把左边剩余的装进新数组
+	        while (low1 <= middle) {
+	            temp[index] = arr[low1++];
+	            index++;
+	        }
+	        // 把右边剩余的装进新数组
+	        while (low2 <= right) {
+	            temp[index] = arr[low2++];
+	            index++;
+	        }
+	        //把新数组里面的有序元素数，装进原数组
+	        for (int i = 0; i < temp.length; i++) {
+	            arr[i + left] = temp[i];
+	        }
+		}	
 		
-	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -135,7 +124,7 @@ public int[] insertSort(int[] arr){
 		//System.out.println("冒泡排序后数组顺序："+Arrays.toString(as.bubbSort(newArr)));
 		//System.out.println("插入排序后数组顺序："+Arrays.toString(as.insertSort(newArr)));
 		//System.out.println("选择排序后的顺序："+Arrays.toString(as.choiceSort(newArr)));
-		as.mergeSort(newArr);
+		as.mergeSort(newArr, 0, arr.length-1);
 		System.out.println("归并排序后的顺序："+Arrays.toString(newArr));
 		
 
